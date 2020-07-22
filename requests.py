bc9c04a5ab5a3a20df212a3ab09da1f2497e0f4c
ClamLq <57548999+ClamLq@users.noreply.github.com>
2020-07-22 14:46:43 +1000

    Add files via upload

---
+++ requests.py
@@ -0,0 +1,13 @@
+import requests 
+import os
+import time
+import sys
+
+class requests():
+    def Getspotifyplaylist():
+        playlistrequest = requests.get('https://api.github.com / users / naveenkrnl') 
+  
+        print(r) 
+        print(r.content) 
+        
+requests.Getspotifyplaylist()
